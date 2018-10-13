c03e52c92031e2806f6e75382e087bf250ed7ed9
Purshottam <purshottam.indore@gmail.com>
2018-10-13 15:51:18 +0530

    Initial Commit

---
+++ components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-360';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+          <Text>
+            React 360 Boilerplate
+          </Text>
+      </View>
+    );
+  }
+};
---
+++ index.js
@@ -0,0 +1,5 @@
+import {
+  AppRegistry,
+} from 'react-360';
+import App from './components/App';
+AppRegistry.registerComponent('App', () => App);
